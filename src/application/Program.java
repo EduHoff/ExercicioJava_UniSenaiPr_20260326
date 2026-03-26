bd356303b902d413a7d3d059d8fff21523fdf700
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2026-03-26 08:33:33 -0300

    Estrutura base para fazer os exercícios

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,66 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import exercises.Exercise;
+import utils.ConsoleUtils;
+
+
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        ConsoleUtils.clear();
+        while (true) {
+            try{
+
+                System.out.print("""
+                ============================================
+                            MENU DE EXERCÍCIOS
+                ============================================
+                1 - Leitura e Exibição 3x3
+                2 - Contagem de Elementos Negativos
+                3 - Soma de Duas Matrizes 4x4
+                4 - Extração da Diagonal Principal
+                5 - Matriz Transposta 3x3
+                6 - Produto de Matrizes (2x3 por 3x2)
+                7 - Verificação de Simetria
+                8 - Médias de Linhas e Colunas 3x4
+                9 - Jogo da Velha
+                0 - Sair
+                ===========================================
+                """);
+
+                System.out.print("||");
+                int num = sc.nextInt();
+
+                if (num == 0) {
+                    break;
+                }
+
+                Class<?> clazz = Class.forName("exercises.Ex" + num);
+                Exercise ex = (Exercise) clazz.getDeclaredConstructor().newInstance();
+                ConsoleUtils.clear();
+                ex.run(sc);
+
+            
+            }catch(InputMismatchException e){
+                ConsoleUtils.clear();
+                System.out.println("Digite apenas números inteiros!\n");
+                sc.nextLine();
+            }catch (ClassNotFoundException e) {
+                ConsoleUtils.clear();
+                System.out.println("Exercício não existe!\n");
+            }catch(Exception e){
+                ConsoleUtils.clear();
+                e.printStackTrace();
+            } 
+        }
+
+        sc.close();
     }
 }
---
+++ src/exercises/Ex1.java
@@ -0,0 +1,18 @@
+package exercises;
+
+import java.util.Scanner;
+
+/* 
+Crie um programa que preencha uma matriz 3x3 com números inteiros
+informados pelo usuário e, em seguida, exiba a matriz.
+*/
+
+public class Ex1 implements Exercise {
+
+    @Override
+    public void run(Scanner sc) {
+        System.out.println("--- Exercício 1 ---\n");
+
+
+    }
+}
